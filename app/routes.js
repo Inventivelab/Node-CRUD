cbad49da3b1f75393a1746bc71e4e0c67c43048d
Said Maadan <saidmaadan@users.noreply.github.com>
2016-10-21 20:37:19 -0500

    Restructured application files and directories

---
+++ app/routes.js
@@ -0,0 +1,9 @@
+const express = require('express'),
+  router = express.Router(),
+  mainController = require('./controllers/main.controller');
+
+  //export router
+module.exports = router;
+
+//define routes
+router.get('/', mainController.showHome);
--- server.js
+++ server.js
@@ -1,15 +1,20 @@
-const express = require('express'),
-  app = express(),
-  port = process.env.PORT || 8080;
+const express     = require('express'),
+  app             = express(),
+  port            = process.env.PORT || 8080,
+  expressLayouts  = require('express-ejs-layouts');
 
   // Configure our application
 
+  // static assets
+  app.use(express.static(__dirname + '/public'));
+
+  //set up templating engine with ejs
+  app.set('view engine', 'ejs');
+  app.use(expressLayouts);
   // set the routes
-  app.get('/', (req, res) =>{
-    res.send('First Website built with MEAN')
-  });
+  app.use(require('./app/routes'));
 
   //start server
   app.listen(port, () => {
-    console.log('App listening on http://localhost:${port}');
+    console.log(`App listening on http://localhost:${port}`);
   });
